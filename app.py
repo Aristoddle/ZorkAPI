9a3d06ae1f23cf327ca087b941a2178621b35367
Joseph Lanzone <j3lanzone@gmail.com>
2019-04-17 10:54:38 -0400

    Cleanup, setting up on local machine

--- app.py
+++ app.py
@@ -11,35 +11,25 @@ import posix
 from flask import request
 import pexpect
 
-# Connect to Redis
+# Connect to Redis 
 #redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 app = Flask(__name__)
 
-#try:
-#    os.mkfifo("zm.pipe")
-#except:
-#    os.system("rm ./zm.pipe")
-#    os.mkfifo("zm.pipe")
-
-#os.system("sleep infinity > zm.pipe &")
-#filename = '/home/J3lanzone/API/zm.pipe'
-#readfifo = os.open(filename, posix.O_NONBLOCK | posix.O_RDONLY)
-#writefifo = os.open(filename, posix.O_NONBLOCK | posix.O_WRONLY)
-#proc = Popen(["/home/J3lanzone/frotz/dfrotz", "/home/J3lanzone/HitchHikers/hhgg.z3"], stdin=writefifo, stdout=PIPE)
 
 global child
-#child = pexpect.spawn('/home/J3lanzone/frotz/dfrotz /home/j3lanzone/HitchHikers/hhgg.z3')
-
-# this start method could be called with args to start different games eventually
 @app.route("/start", methods=['GET', 'POST'])
 def start():
     global child
     game = request.args.get('game')
     os.setuid(1000)
+    
     if (game == 'zork'):
+        child = pexpect.spawn('/home/J3lanzone/frotz/dfrotz /home/J3lanzone/Zork1/zork1.z5')
+    elif (game == 'hike'):
         child = pexpect.spawn('/home/J3lanzone/frotz/dfrotz /home/J3lanzone/HitchHikers/hhgg.z3')
     else:
-        child = pexpect.spawn('/home/J3lanzone/frotz/dfrotz /home/J3lanzone/HitchHikers/hhgg.z3')
+         child = pexpect.spawn('/home/J3lanzone/frotz/dfrotz /home/J3lanzone/Zork1/zork1.z5')
+
     return(f"loaded {game}")
 
 @app.route("/check", methods=['GET', 'POST'])
