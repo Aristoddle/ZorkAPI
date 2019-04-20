e559dbbd64c4b61223b29ef9a06d932c5673d0aa
Unknown <j3lanzone@gmail.com>
2019-04-19 21:44:01 -0400

    Messing w. multiThreading

--- app.py
+++ app.py
@@ -8,77 +8,76 @@ from string import Template
 from subprocess import PIPE, Popen
 
 import pexpect
-from flask import Flask, request
+from flask import Flask, request, session
+from flask_session import Session
 from redis import Redis, RedisError
 
-global child
+SESSION_TYPE = 'filesystem'
 
 # Connect to Redis 
-#redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
+# redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 app = Flask(__name__)
+app.config.from_object(__name__)
+Session(app)
 
 @app.route("/start", methods=['GET', 'POST'])
 def start():
-    global child
     game = request.args.get('game')
     os.setuid(1000)
 
     try:
-        child.close()
+        session.get("gameThread", None).close()
     
     except:
         pass
 
     if (game == 'hike'):
-        child = pexpect.spawn('/home/J3lanzone/frotz/dfrotz -mp /home/J3lanzone/Games/HitchHikers/hhgg.z3')
+        session["gameThread"] = pexpect.spawn('/home/J3lanzone/frotz/dfrotz -mp /home/J3lanzone/Games/HitchHikers/hhgg.z3')
         game = "The Hitchiker\'s Guide to the Galaxy"
     
     elif (game == 'spell'):
-        child = pexpect.spawn('/home/J3lanzone/frotz/dfrotz -mp /home/J3lanzone/Games/Spellbreaker/spellbre.dat') 
+        session["gameThread"] = pexpect.spawn('/home/J3lanzone/frotz/dfrotz -mp /home/J3lanzone/Games/Spellbreaker/spellbre.dat') 
         game = "Spellbreaker"
 
     elif (game == 'wish'):
-        child = pexpect.spawn('/home/J3lanzone/frotz/dfrotz -mp /home/J3lanzone/Games/Wishbringer/wishbrin.dat')
+        session["gameThread"] = pexpect.spawn('/home/J3lanzone/frotz/dfrotz -mp /home/J3lanzone/Games/Wishbringer/wishbrin.dat')
         game = "Wishbringer"
 
     elif (game == 'zork1'):
-        child = pexpect.spawn('/home/J3lanzone/frotz/dfrotz -mp /home/J3lanzone/Games/Zork1/zork1.z5')
+        session["gameThread"] = pexpect.spawn('/home/J3lanzone/frotz/dfrotz -mp /home/J3lanzone/Games/Zork1/zork1.z5')
         game = "Zork One"
 
     elif (game == 'zork2'):
-        child = pexpect.spawn('/home/J3lanzone/frotz/dfrotz -mp /home/J3lanzone/Games/Zork2/zork2.dat')
+        session["gameThread"] = pexpect.spawn('/home/J3lanzone/frotz/dfrotz -mp /home/J3lanzone/Games/Zork2/zork2.dat')
         game = "Zork Two"
 
     elif (game == 'zork3'):
-        child = pexpect.spawn('/home/J3lanzone/frotz/dfrotz -mp /home/J3lanzone/Games/Zork3/ZORK3.DAT')
+        session["gameThread"] = pexpect.spawn('/home/J3lanzone/frotz/dfrotz -mp /home/J3lanzone/Games/Zork3/ZORK3.DAT')
         game = "Zork Three"
 
     else:
-         child = pexpect.spawn('/home/J3lanzone/frotz/dfrotz -mp /home/J3lanzone/Zork3/zork1.z5')
+         session["gameThread"] = pexpect.spawn('/home/J3lanzone/frotz/dfrotz -mp /home/J3lanzone/Games/Zork1/zork1.z5')
 
-    child.expect('Serial [n|N]umber [0-9]+')
-    firstLine = child.before.decode('utf-8')
-    after = child.after.decode('utf-8')
+    session.get("gameThread", None).expect('Serial [n|N]umber [0-9]+')
+    firstLine = session.get("gameThread", None).before.decode('utf-8')
+    after = session.get("gameThread", None).after.decode('utf-8')
 
-    return(f"Loaded {game}.\n Before: {firstLine} {after}")
+    return(f"{firstLine} {after}")
 
 @app.route("/check", methods=['GET', 'POST'])
 def check():
-    global child
-    child.expect('>')
-    return(child.before.decode('utf-8'))
+    session.get("gameThread", None).expect('>')
+    return(session.get("gameThread", None).before.decode('utf-8'))
     
 @app.route("/action", methods=['GET', 'POST'])
 def action():
-    global child
     command = request.args.get('cmd') 
-    child.sendline(command)
-    child.expect('>')
-    output = child.before.decode('utf-8')
+    session.get("gameThread", None).sendline(command)
+    session.get("gameThread", None).expect('>')
+    output = session.get("gameThread", None).before.decode('utf-8')
     output = output.replace(command,"",1)
     return(output)
 
 if __name__ == "__main__":
     #start()
     app.run(host='0.0.0.0', port=443)
-    global child
